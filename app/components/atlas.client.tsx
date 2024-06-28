1a94387715f12b134edb740f498c2fd156548be8
David Hochbaum <dhochbaum@planning.nyc.gov>
2024-06-28 17:45:32 -0400

    Added map constraints

--- app/components/atlas.client.tsx
+++ app/components/atlas.client.tsx
@@ -1,11 +1,20 @@
 import { DeckGL } from "@deck.gl/react";
 import { Map } from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
+import { useState } from "react";
 import {
   useCapitalProjectsLayer,
   useCommunityDistrictsLayer,
   useCityCouncilDistrictsLayer,
 } from "./layers";
+
+export type ViewState = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+  bearing: number;
+  pitch: number;
+};
 
 const INITIAL_VIEW_STATE = {
   longitude: -74.0008,
@@ -15,13 +24,37 @@ const INITIAL_VIEW_STATE = {
   pitch: 0,
 };
 
+const MAX_ZOOM = 20;
+const MIN_ZOOM = 10;
+
 export function Atlas() {
   const capitalProjectsLayer = useCapitalProjectsLayer();
   const communityDistrictsLayer = useCommunityDistrictsLayer();
   const cityCouncilDistrictsLayer = useCityCouncilDistrictsLayer();
+
+  const [viewState, setViewState] = useState<ViewState>(INITIAL_VIEW_STATE);
+
   return (
     <DeckGL
-      initialViewState={INITIAL_VIEW_STATE}
+      viewState={viewState}
+      onViewStateChange={(updatedViewState: any) => {
+        setViewState({
+          longitude: Math.min(
+            -73.6311,
+            Math.max(-74.3308, updatedViewState.viewState.longitude),
+          ),
+          latitude: Math.min(
+            41.103,
+            Math.max(40.2989, updatedViewState.viewState.latitude),
+          ),
+          bearing: updatedViewState.viewState.bearing,
+          pitch: updatedViewState.viewState.pitch,
+          zoom: Math.min(
+            MAX_ZOOM,
+            Math.max(MIN_ZOOM, updatedViewState.viewState.zoom),
+          ),
+        });
+      }}
       controller={true}
       style={{ height: "100vh", width: "100vw" }}
       layers={[
