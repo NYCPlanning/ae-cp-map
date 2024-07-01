94c25df0dcaea617af847c2e90fc7ef7f649ebb0
tangoyankee <miller.tim108@gmail.com>
2024-07-01 16:42:16 -0400

    test changing the option

--- app/components/ui/AdminDropDown/index.test.tsx
+++ app/components/ui/AdminDropDown/index.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
 import { AdminDropDown } from ".";
 
 describe("AdminDropDown", () => {
@@ -13,21 +14,65 @@ describe("AdminDropDown", () => {
     expect(screen.getByText("Test option")).toBeInTheDocument();
   });
 
-  it.only("should render with default select option", () => {
+  it("should render with default select option", () => {
     render(
       <AdminDropDown formId="testBoundary" formLabel="Test Boundary">
         <option value="test-option">Test option</option>
       </AdminDropDown>,
     );
 
-    expect(screen.getByLabelText("Test Boundary")).toBeInTheDocument();
-    expect(screen.getByText("Test option")).toBeInTheDocument();
     const options: Array<HTMLOptionElement> = screen.getAllByRole("option");
-    console.debug(
-      options.map((option) => console.debug("is selected", option.selected)),
+    const defaultSelect = options.find((option) => option.value === "");
+    expect(defaultSelect).toBeInTheDocument();
+    expect(defaultSelect?.selected).toBe(true);
+    const testSelect = options.find((option) => option.value === "test-option");
+    expect(testSelect).toBeInTheDocument();
+    expect(testSelect?.selected).toBe(false);
+  });
+
+  it("should render with test select option", () => {
+    render(
+      <AdminDropDown
+        formId="testBoundary"
+        formLabel="Test Boundary"
+        selectValue="test-option"
+      >
+        <option value="test-option">Test option</option>
+      </AdminDropDown>,
     );
-    // screen.debug();
-    // console.debug("option", option);
-    // expect(option).toHaveValue("");
+
+    const options: Array<HTMLOptionElement> = screen.getAllByRole("option");
+    const defaultSelect = options.find((option) => option.value === "");
+    expect(defaultSelect).toBeInTheDocument();
+    expect(defaultSelect?.selected).toBe(false);
+    const testSelect = options.find((option) => option.value === "test-option");
+    expect(testSelect).toBeInTheDocument();
+    expect(testSelect?.selected).toBe(true);
+  });
+
+  it.todo("should call function when form label is clicked");
+
+  it.only("should call select function with null when changing to default select option", async () => {
+    const onSelectValueChange = vi.fn();
+    render(
+      <AdminDropDown
+        formId="testBoundary"
+        formLabel="Test Boundary"
+        selectValue="test-option"
+        onSelectValueChange={onSelectValueChange}
+      >
+        <option value="test-option">Test option</option>
+      </AdminDropDown>,
+    );
+
+    // Establish default option is not selected
+    const options: Array<HTMLOptionElement> = screen.getAllByRole("option");
+    const defaultSelect = options.find((option) => option.value === "");
+    expect(defaultSelect).toBeInTheDocument();
+    expect(defaultSelect?.selected).toBe(false);
+
+    // Select the default option
+    await userEvent.selectOptions(screen.getByRole("combobox"), "");
+    expect(onSelectValueChange).toHaveBeenCalledWith(null);
   });
 });
