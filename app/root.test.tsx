bdcdf2ea0bf3b93b66c4536ef58e544ce4a44fa0
tangoyankee <miller.tim108@gmail.com>
2024-07-02 16:59:25 -0400

    errors when testing atlas in remix stub

--- app/root.test.tsx
+++ app/root.test.tsx
@@ -1,3 +1,18 @@
-describe("App component", () => {});
+import { createRemixStub } from "@remix-run/testing";
+import App, { loader } from "./root";
+import { render, screen, waitFor } from "@testing-library/react";
 
-describe("App loader", () => {});
+describe("App component", () => {
+  it.only("should render", async () => {
+    const AppStub = createRemixStub([
+      {
+        path: "/",
+        Component: App,
+        loader: loader,
+      },
+    ]);
+    render(<AppStub />);
+    await waitFor(() => screen.getByText("Borough"));
+    screen.debug();
+  });
+});
