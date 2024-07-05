529d5b0402a93784f5cf397d2c1cf5a3b55afa74
tangoyankee <miller.tim108@gmail.com>
2024-07-05 14:34:05 -0400

    district type dropdown unit tests

--- app/components/AdminDropdown/DistrictTypeDropdown.test.tsx
+++ app/components/AdminDropdown/DistrictTypeDropdown.test.tsx
@@ -1,4 +1,20 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { DistrictTypeDropdown } from "./DistrictTypeDropdown";
+import userEvent from "@testing-library/user-event";
+
 describe("DistrictTypeDropdown", () => {
-  it.todo("should render district type form details and options");
-  it.todo("should update search params when changing the district type");
+  it("should render district type form details and options", () => {
+    const updateSearchParams = vi.fn();
+    render(<DistrictTypeDropdown updateSearchParams={updateSearchParams} />);
+    expect(screen.getByLabelText("District Type")).toBeInTheDocument();
+    expect(screen.getByText("Community District")).toBeInTheDocument();
+    expect(screen.getByText("City Council District")).toBeInTheDocument();
+  });
+
+  it("should update search params when changing the district type", async () => {
+    const updateSearchParams = vi.fn();
+    render(<DistrictTypeDropdown updateSearchParams={updateSearchParams} />);
+    await userEvent.selectOptions(screen.getByRole("combobox"), "cd");
+    expect(updateSearchParams).toHaveBeenCalledWith({ districtType: "cd" });
+  });
 });
