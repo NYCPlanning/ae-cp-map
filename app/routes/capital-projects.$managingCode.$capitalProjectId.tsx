83e17c448717102c79526c3737b988687c4f45ba
Tyler Matteo <tylermatteo434@gmail.com>
2024-07-07 13:26:26 -0400

    Add capital project detail panel and connect it to route for single project

--- app/routes/capital-projects.$managingCode.$capitalProjectId.tsx
+++ app/routes/capital-projects.$managingCode.$capitalProjectId.tsx
@@ -1,3 +1,43 @@
+import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { useLoaderData, useNavigate, useSearchParams } from "@remix-run/react";
+import {
+  findCapitalProjectByManagingCodeCapitalProjectId,
+  findAgencies,
+} from "../gen";
+import { CapitalProjectDetailPanel } from "../components/CapitalProjectDetailPanel";
+
+export async function loader({ params }: LoaderFunctionArgs) {
+  const { managingCode, capitalProjectId } = params;
+  const agenciesResponse = await findAgencies({
+    baseURL: `${import.meta.env.VITE_ZONING_API_URL}/api`,
+  });
+  if (managingCode === undefined || capitalProjectId === undefined) {
+    throw json("Bad Request", { status: 400 });
+  }
+  const capitalProject = await findCapitalProjectByManagingCodeCapitalProjectId(
+    managingCode,
+    capitalProjectId,
+    {
+      baseURL: `${import.meta.env.VITE_ZONING_API_URL}/api`,
+    },
+  );
+  return json({ capitalProject, agencies: agenciesResponse.agencies });
+}
+
 export default function CapitalProject() {
-  return <></>;
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const { capitalProject, agencies } = useLoaderData<typeof loader>();
+  return (
+    <CapitalProjectDetailPanel
+      capitalProject={capitalProject}
+      agencies={agencies}
+      onClose={() => {
+        navigate({
+          pathname: "/",
+          search: `?${searchParams.toString()}`,
+        });
+      }}
+    />
+  );
 }
