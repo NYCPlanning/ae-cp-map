8e2afba38818e37c00e3715e9d115721350231b2
pratishta <ypradhi9@gmail.com>
2024-07-10 11:53:49 -0400

    Working logging of projects

--- app/routes/city-council-districts.$cityCouncilDistrictId.capital-projects.tsx
+++ app/routes/city-council-districts.$cityCouncilDistrictId.capital-projects.tsx
@@ -2,27 +2,30 @@ import { LoaderFunctionArgs, json } from "@remix-run/node";
 import { findCapitalProjectsByCityCouncilId } from "../gen";
 import { useLoaderData, useNavigate } from "@remix-run/react";
 
-export async function loader({params}:  LoaderFunctionArgs) {
-    console.log("in loader");
-    const { cityCouncilDistrictId } = params;
-    if (cityCouncilDistrictId === undefined) {
-        throw json("Bad Request", { status: 400 });
-    }
-    const projectsByCityCouncilDistrictResponse = await findCapitalProjectsByCityCouncilId(
-        cityCouncilDistrictId, 
-        {
-            baseURL: `${import.meta.env.VITE_ZONING_API_URL}/api`,
-        },
+export async function loader({ params }: LoaderFunctionArgs) {
+  console.log("in loader");
+  const { cityCouncilDistrictId } = params;
+  if (cityCouncilDistrictId === undefined) {
+    throw json("Bad Request", { status: 400 });
+  }
+  const projectsByCityCouncilDistrictResponse =
+    await findCapitalProjectsByCityCouncilId(
+      cityCouncilDistrictId,
+      {
+        limit: 10,
+        offset: 1,
+      },
+      {
+        baseURL: `${import.meta.env.VITE_ZONING_API_URL}/api`,
+      },
     );
-    console.log(projectsByCityCouncilDistrictResponse);
+  console.log(projectsByCityCouncilDistrictResponse);
 
-    return json({projectsByCityCouncilDistrictResponse});
+  return json({ projectsByCityCouncilDistrictResponse });
 }
 
 export default function CapitalProjectsByCityCouncilDistrict() {
-    const projectsByCityCouncilDistrict = useLoaderData<typeof loader>();
-    console.log(projectsByCityCouncilDistrict);
-    return (
-        <p>projects by city council district</p>
-    );
+  const projectsByCityCouncilDistrict = useLoaderData<typeof loader>();
+  console.log(projectsByCityCouncilDistrict);
+  return <p>projects by city council district</p>;
 }
