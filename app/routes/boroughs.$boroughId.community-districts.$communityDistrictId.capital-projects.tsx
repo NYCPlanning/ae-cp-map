be6b4ddb055efbef50161a6944b17b397263dad5
tangoyankee <miller.tim108@gmail.com>
2024-08-21 11:56:38 -0400

    Capital Projects in Community District
    Implement the panel to show capital projects in a community district
    
    closes #15

--- app/routes/boroughs.$boroughId.community-districts.$communityDistrictId.capital-projects.tsx
+++ app/routes/boroughs.$boroughId.community-districts.$communityDistrictId.capital-projects.tsx
@@ -1,3 +1,88 @@
+import { Flex } from "@nycplanning/streetscape";
+import { json, LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { CapitalProjectsPanel } from "~/components/CapitalProjectsList";
+import { Pagination } from "~/components/Pagination";
+import {
+  findAgencies,
+  findBoroughs,
+  findCapitalProjectsByBoroughIdCommunityDistrictId,
+} from "~/gen";
+
+export async function loader({ request, params }: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+  const itemsPerPage = 7;
+  const pageParam = url.searchParams.get("page");
+  const page = pageParam === null ? 1 : parseInt(pageParam);
+
+  const { boroughId, communityDistrictId } = params;
+  if (
+    boroughId === undefined ||
+    communityDistrictId === undefined ||
+    isNaN(page)
+  ) {
+    throw json("Bad request", { status: 400 });
+  }
+
+  const offset = (page - 1) * itemsPerPage;
+  const projectsByCommunityDistrictPromise =
+    findCapitalProjectsByBoroughIdCommunityDistrictId(
+      boroughId,
+      communityDistrictId,
+      {
+        limit: itemsPerPage,
+        offset: offset,
+      },
+      {
+        baseURL: `${import.meta.env.VITE_ZONING_API_URL}/api`,
+      },
+    );
+
+  const agenciesPromise = findAgencies({
+    baseURL: `${import.meta.env.VITE_ZONING_API_URL}/api`,
+  });
+  const boroughsPromise = findBoroughs({
+    baseURL: `${import.meta.env.VITE_ZONING_API_URL}/api`,
+  });
+  const [agenciesResponse, boroughsResponse, capitalProjectsResponse] =
+    await Promise.all([
+      agenciesPromise,
+      boroughsPromise,
+      projectsByCommunityDistrictPromise,
+    ]);
+  const boroughAbbr = boroughsResponse.boroughs.find(
+    (borough) => borough.id === boroughId,
+  )?.abbr;
+  return {
+    capitalProjectsResponse,
+    agencies: agenciesResponse.agencies,
+    boroughAbbr,
+    communityDistrictId,
+  };
+}
+
 export default function CapitalProjectsByBoroughIdCommunityDistrictId() {
-  return <></>;
+  const {
+    capitalProjectsResponse: { total: capitalProjectsTotal, capitalProjects },
+    agencies,
+    boroughAbbr,
+    communityDistrictId,
+  } = useLoaderData<typeof loader>();
+
+  return (
+    <CapitalProjectsPanel
+      capitalProjects={capitalProjects}
+      agencies={agencies}
+      district={`Community District ${boroughAbbr}${communityDistrictId}`}
+    >
+      <Flex
+        paddingTop={4}
+        alignItems="center"
+        justifyContent={"space-between"}
+        marginTop={"auto"}
+      >
+        <Pagination total={capitalProjectsTotal} />
+      </Flex>
+    </CapitalProjectsPanel>
+  );
 }
