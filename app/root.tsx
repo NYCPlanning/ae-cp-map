925ff0747b9bbffad4af1cb2fde7cacfd1324652
David Hochbaum <dhochbaum@planning.nyc.gov>
2025-02-12 09:34:42 -0500

    Search button disabled when no valid selection has been made

--- app/root.tsx
+++ app/root.tsx
@@ -298,6 +298,7 @@ export default function App() {
                     width="full"
                     onClick={() => performNewSearch(newPath())}
                     mt={0}
+                    isDisabled={!districtId && !managingAgency ? true : false}
                   >
                     Search
                   </Button>
