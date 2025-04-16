0389093f1bc6eea91b878bc3e35865f14d6a2cca
tangoyankee <miller.tim108@gmail.com>
2025-04-16 18:17:50 -0400

    snake_case attributes

--- app/components/layers/useCommunityDistrictsLayer.client.tsx
+++ app/components/layers/useCommunityDistrictsLayer.client.tsx
@@ -2,9 +2,9 @@ import { MVTLayer } from "@deck.gl/geo-layers";
 import { useSearchParams } from "@remix-run/react";
 
 export interface CommunityDistrictProperties {
-  boroughIdCommunityDistrictId: string;
+  borough_id_community_district_id: string;
   layerName: string;
-  abbr: string | null;
+  borough_abbr: string | null;
 }
 
 export function useCommunityDistrictsLayer() {
@@ -17,20 +17,23 @@ export function useCommunityDistrictsLayer() {
       `${import.meta.env.VITE_ZONING_API_URL}/api/community-districts/{z}/{x}/{y}.pbf`,
     ],
     visible: districtType === "cd",
-    uniqueIdProperty: "boroughIdCommunityDistrictId",
+    uniqueIdProperty: "borough_id_community_district_id",
     pickable: true,
     getPointRadius: 5,
     filled: false,
+    maxZoom: 13,
     getLineColor: [113, 128, 150, 255],
     getLineWidth: 3,
     lineWidthUnits: "pixels",
     pointType: "text",
     getText: ({ properties }: { properties: CommunityDistrictProperties }) => {
       // If CommunityDistrictId > 18, the area represents a Park, not a Community District
-      if (parseInt(properties.boroughIdCommunityDistrictId.slice(-2)) > 18) {
+      if (
+        parseInt(properties.borough_id_community_district_id.slice(-2)) > 18
+      ) {
         return null;
       }
-      return `${properties.abbr} ${parseInt(properties.boroughIdCommunityDistrictId.slice(-2))}`;
+      return `${properties.borough_abbr} ${parseInt(properties.borough_id_community_district_id.slice(-2))}`;
     },
     getTextColor: [98, 98, 98, 255],
     textFontFamily: "Helvetica Neue, Arial, sans-serif",
