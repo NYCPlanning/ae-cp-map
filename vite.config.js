1c8242b145976e31d9637fcb4a7081a9667f12e2
horatio <horatiorosa@gmail.com>
2025-06-25 09:20:49 -0400

    FF remove installGlobals

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import path from "path";
-
-installGlobals();
 
 export default defineConfig({
   plugins: [
     !process.env.VITEST &&
       remix({
-        ignoredRouteFiles: ["**/?(*.)+(spec|test).[tj]s?(x)"],
+        ignoredRouteFiles: ["**/?(*.)+(spec|test).[tj]s?(x)", "**/*.css"],
       }),
   ],
   resolve: {
