ac350ed5c1af56b6fa9927c55372e41d630b23c9
Tyler Matteo <tylermatteo434@gmail.com>
2025-07-11 13:12:10 -0400

    Remove installGlobals()

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import path from "path";
-
-installGlobals();
 
 export default defineConfig({
   plugins: [
