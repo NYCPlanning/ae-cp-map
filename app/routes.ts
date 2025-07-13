988bc5e0c42f9ecb07f1187da13b1014dd7e1f86
Tyler Matteo <tylermatteo434@gmail.com>
2025-07-13 11:10:50 -0400

    Switch routing over to config based without moving or renaming files

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,28 @@
-import { flatRoutes } from "@remix-run/fs-routes";
+import { type RouteConfig } from "@remix-run/route-config";
+import { remixRoutesOptionAdapter } from "@remix-run/routes-option-adapter";
 
-export default flatRoutes();
+export default remixRoutesOptionAdapter((defineRoutes) => {
+  return defineRoutes((route) => {
+    route("capital-projects", "routes/capital-projects.tsx");
+    route(
+      "capital-projects/:managingCode/:capitalProjectId",
+      "routes/capital-projects_.$managingCode.$capitalProjectId.tsx",
+    );
+    route(
+      "boroughs/:boroughId/community-districts/:communityDistrictId/capital-projects/:managingCode/:capitalProjectId",
+      "routes/boroughs.$boroughId.community-districts.$communityDistrictId.capital-projects_.$managingCode.$capitalProjectId.tsx",
+    );
+    route(
+      "boroughs/:boroughId/community-districts/:communityDistrictId/capital-projects",
+      "routes/boroughs.$boroughId.community-districts.$communityDistrictId.capital-projects.tsx",
+    );
+    route(
+      "city-council-districts/:cityCouncilDistrictId/capital-projects/:managingCode/:capitalProjectId",
+      "routes/city-council-districts.$cityCouncilDistrictId.capital-projects_.$managingCode.$capitalProjectId.tsx",
+    );
+    route(
+      "city-council-districts/:cityCouncilDistrictId/capital-projects",
+      "routes/city-council-districts.$cityCouncilDistrictId.capital-projects.tsx",
+    );
+  });
+}) satisfies RouteConfig;
