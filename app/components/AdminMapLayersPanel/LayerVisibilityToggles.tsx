448c86c5fa26b384382d377d9c4cddab3251869e
pratishta <ypradhi9@gmail.com>
2025-10-15 15:27:01 -0400

    Show cbbrs, add legend and toggle
    Show all cbbrs
    
    Playing with icon layers
    
    ugh some ugly icons showing up after converting education to uri
    
    manually create svgs
    
    Add svgs and conditionally render them
    
    added CBBR switch to capital planning layers panel
     - Add Legend Compoent with Icons
    
    Small edits
    
    Coordinate toggle with cbbr layer
    
    Clean up
    
    Provide fill color as is without function
    
    added 2xl styling to parent grid, children and layer visibility toggles
    
    refactor to remove unnecessary helper methods and move functionality to update params to show/hide layers from root to LayerVisibilityToggles component
    
    restored package-lock to version on main

--- app/components/AdminMapLayersPanel/LayerVisibilityToggles.tsx
+++ app/components/AdminMapLayersPanel/LayerVisibilityToggles.tsx
@@ -11,19 +11,26 @@ import {
   Tooltip,
   useBreakpointValue,
 } from "@nycplanning/streetscape";
+import { useUpdateSearchParams } from "../../utils/utils";
 
-export type LayerVisibilityTogglesProps = {
-  capitalProjectsOn: boolean;
-  onCapitalProjectsToggle: (next: boolean) => void;
-};
+export function LayerVisibilityToggles() {
+  const [searchParams, updateSearchParams] = useUpdateSearchParams();
 
-export function LayerVisibilityToggles({
-  capitalProjectsOn,
-  onCapitalProjectsToggle,
-}: LayerVisibilityTogglesProps) {
+  const capitalProjectsOn = searchParams.get("capitalProjects") !== "off";
+  const cbbrOn = searchParams.get("cbbr") !== "off";
+
+  const setCapitalProjects = (next: boolean) =>
+    updateSearchParams({ capitalProjects: next ? undefined : "off" });
+
+  const setCbbr = (next: boolean) =>
+    updateSearchParams({ cbbr: next ? undefined : "off" });
+
   const capitalProjectsTooltipCopy = `New York City’s potential, planned, and ongoing capital projects.
   Unmapped projects, such as the purchase of vehicles or digital infrastructure, are not included in this tool.
   `;
+  const capitalProjectsBudgetTooltipCopy = `Every year, boards submit prioritized  capital budget requests that address local needs.
+    Expense requests are not included in this tool. All capital budget requests, mapped and unmapped, are included.
+   `;
 
   const placement =
     useBreakpointValue<PlacementWithLogical>({
@@ -32,61 +39,135 @@ export function LayerVisibilityToggles({
     }) ?? "bottom";
 
   return (
-    <Box>
-      <FormControl
-        display="flex"
-        alignItems="flex-start"
-        justifyContent="space-between"
-        gap={3}
-        paddingTop={2}
-      >
-        <HStack spacing={3}>
-          <Switch
-            id="capital-projects-toggle"
-            isChecked={capitalProjectsOn}
-            onChange={(e) => onCapitalProjectsToggle(e.target.checked)}
-          />
-          <FormLabel htmlFor="capital-projects-toggle" padding={"0"}>
-            <Heading as="h2" fontWeight="medium" fontSize="md" color="gray.600">
-              Capital Projects
-            </Heading>
-          </FormLabel>
-        </HStack>
-
-        <HStack spacing={3} alignItems="flex-start">
-          <Tooltip
-            hasArrow
-            label={capitalProjectsTooltipCopy}
-            maxWidth={"15rem"}
-            placement={placement}
+    <>
+      <Box display={"flex"} flexDirection={"column"} gap={2}>
+        <Box>
+          <FormControl
+            display="flex"
+            alignItems="flex-start"
+            justifyContent="space-between"
+            gap={3}
+            paddingTop={2}
           >
-            <IconButton
-              aria-label="information about the capital planning portal"
-              icon={<InfoIcon boxSize={5} verticalAlign={"top"} />}
-              size="sm"
-              isRound
-              borderColor={"white"}
-              borderStyle="solid"
-              border={"none"}
-              color={"gray.600"}
-              minHeight="auto"
-              minWidth="auto"
-              backgroundColor={"white"}
-              verticalAlign={"top"}
-              _hover={{
-                bg: "blackAlpha.100",
-              }}
-              _focus={{ borderColor: "white" }}
-            />
-          </Tooltip>
-          <Box
-            height={"1.25rem"}
-            width={"1.25rem"}
-            background={"brand.800"}
-            borderRadius={"5px"}
-          ></Box>
-        </HStack>
-      </FormControl>
-    </Box>
+            <HStack alignItems={"flex-start"}>
+              <Switch
+                id="capital-projects-toggle"
+                isChecked={capitalProjectsOn}
+                onChange={(e) => setCapitalProjects(e.target.checked)}
+              />
+              <FormLabel htmlFor="capital-projects-toggle" padding={"0"}>
+                <Heading
+                  as="h2"
+                  fontWeight="medium"
+                  fontSize={{ base: "md", "2xl": "sm" }}
+                  color="gray.600"
+                >
+                  Capital Projects
+                </Heading>
+              </FormLabel>
+            </HStack>
+
+            <HStack spacing={3} alignItems="flex-start">
+              <Tooltip
+                hasArrow
+                label={capitalProjectsTooltipCopy}
+                maxWidth={"15rem"}
+                placement={placement}
+              >
+                <IconButton
+                  aria-label="information about the capital planning portal"
+                  icon={<InfoIcon boxSize={5} verticalAlign={"top"} />}
+                  size="sm"
+                  isRound
+                  borderColor={"white"}
+                  borderStyle="solid"
+                  border={"none"}
+                  color={"gray.600"}
+                  minHeight="auto"
+                  minWidth="auto"
+                  backgroundColor={"white"}
+                  verticalAlign={"top"}
+                  _hover={{
+                    bg: "blackAlpha.100",
+                  }}
+                  _focus={{ borderColor: "white" }}
+                />
+              </Tooltip>
+              <Box
+                height={"1.25rem"}
+                width={"1.25rem"}
+                background={"brand.800"}
+                borderRadius={"5px"}
+              ></Box>
+            </HStack>
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl
+            display="flex"
+            alignItems="flex-start"
+            justifyContent="space-between"
+            gap={3}
+          >
+            <HStack alignItems={"flex-start"}>
+              <Switch
+                id="cb-capital-budget-requests"
+                isChecked={cbbrOn}
+                onChange={(e) => setCbbr(e.target.checked)}
+              />
+              <FormLabel
+                htmlFor="cb-capital-budget-requests"
+                mb="0"
+                width={{ base: "40dvw", md: "12dvw", "2xl": "5dvw" }}
+              >
+                <Heading
+                  as="h2"
+                  fontWeight="medium"
+                  fontSize={{ base: "md", "2xl": "sm" }}
+                  color="gray.600"
+                >
+                  Community Board Capital Budget Requests
+                </Heading>
+              </FormLabel>
+            </HStack>
+
+            <HStack spacing={3} alignItems={"flex-start"}>
+              <Tooltip
+                hasArrow
+                label={capitalProjectsBudgetTooltipCopy}
+                maxWidth={"15rem"}
+                placement={placement}
+              >
+                <IconButton
+                  aria-label="information about the capital planning budget requests"
+                  icon={<InfoIcon boxSize={5} verticalAlign={"top"} />}
+                  size="sm"
+                  isRound
+                  borderColor={"white"}
+                  borderStyle="solid"
+                  border={"none"}
+                  color={"gray.600"}
+                  minHeight="auto"
+                  minWidth="auto"
+                  backgroundColor={"white"}
+                  verticalAlign={"top"}
+                  _hover={{
+                    bg: "blackAlpha.100",
+                  }}
+                  _focus={{ borderColor: "white" }}
+                  onClick={() => console.log("hello")}
+                />
+              </Tooltip>
+              <Box
+                height={"1.25rem"}
+                width={"1.25rem"}
+                background={"#2B6CB0;"}
+                borderRadius={"5px"}
+              ></Box>
+            </HStack>
+          </FormControl>
+        </Box>
+      </Box>
+    </>
   );
 }
--- app/root.tsx
+++ app/root.tsx
@@ -162,9 +162,9 @@ export default function App() {
     initFullStoryAnalytics();
   }, []);
   const [viewState, setViewState] = useState<MapViewState>(INITIAL_VIEW_STATE);
-  const [, setSearchParams] = useSearchParams();
-  const [, updateSearchParams] = useUpdateSearchParams();
-  const [showCapitalProjects, setShowCapitalProjects] = useState(true);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const showCapitalProjects = searchParams.get("capitalProjects") !== "off";
+  const showCbbr = searchParams.get("cbbr") !== "off";
 
   const {
     boroughs,
@@ -207,6 +207,7 @@ export default function App() {
                   viewState={viewState}
                   setViewState={(MapViewState) => setViewState(MapViewState)}
                   showCapitalProjects={showCapitalProjects}
+                  showCbbr={showCbbr}
                 />{" "}
                 <Grid
                   templateColumns={{
@@ -214,12 +215,15 @@ export default function App() {
                     md: "1.5dvw [col-start] 1fr repeat(10, 1fr) 1fr [col-end] 1.5dvw",
                     lg: "1.18dvw [col-start] 1fr repeat(10, 1fr) 1fr [col-end] 1.18dvw",
                     xl: "0.86dvw [col-start] 1fr repeat(10, 1fr) 1fr [col-end] 0.86dvw",
+                    "2xl":
+                    "0.8dvw [col-start] 1fr repeat(10, 1fr) 1fr [col-end] 0.82dvw",
                   }}
                   gap={{
                     base: "0 3dvw",
                     md: "0 1.6dvw",
                     lg: "0 1.22dvw",
                     xl: "0 0.94dw",
+                    "2xl": "0 0.78dw",
                   }}
                   templateRows={{
                     base: "7dvh 2dvh [row-start] 1fr [row-end] 2dvh 7dvh",
@@ -236,6 +240,7 @@ export default function App() {
                       base: "col-start / span 7",
                       md: "col-start / span 4",
                       xl: "col-start / span 3",
+                      "2xl": "col-start / span 2",
                     }}
                     gridRow={{
                       base: "row-start / row-end",
@@ -273,10 +278,7 @@ export default function App() {
                         width={"100%"}
                       >
                         <MapLayersPanel>
-                          <LayerVisibilityToggles
-                            capitalProjectsOn={showCapitalProjects}
-                            onCapitalProjectsToggle={setShowCapitalProjects}
-                          />
+                          <LayerVisibilityToggles />
                           <SearchByAttributeMenu
                             agencies={managingAgencies}
                             projectTypes={agencyBudgets}
@@ -297,6 +299,7 @@ export default function App() {
                       base: "1 / -1",
                       md: "9 / span 5",
                       xl: "10 / col-end",
+                      "2xl": "11 / col-end",
                     }}
                     gridRow={{
                       base: "3 / -1",
